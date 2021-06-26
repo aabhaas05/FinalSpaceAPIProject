79b1e73957c321eab77c7b05bcb9d55c5d96aae9
Aabhaas Batra <55681126+aabhaas05@users.noreply.github.com>
2021-06-26 11:22:32 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import './App.css';
+// import Greet from './components/Greet';
+// import Welcome from './components/Welcome';
+// import Counter from './components/Counter';
+// import EventBind from './components/EventBind';
+//import ParentComponent from './components/ParentComponent';
+//import UserGreeting from './components/UserGreeting';
+// import GrandParent from './components/GrandParent';
+// import { UserProvider } from './components/userContext';
+import PostList from './components/PostList';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Greet name="JavaScript"><Welcome /></Greet>
+      <Greet name="Golang" />
+      <Greet name="Swift"><h1>Language of ios Apps</h1></Greet>
+      <Welcome name="Flutter" />
+      <Welcome name="React Native"/>
+      <Welcome name="Java"><button>Click</button></Welcome> */}
+      {/* <Counter /> */}
+      {/* <EventBind /> */}
+      {/* <ParentComponent /> */}
+      {/* <UserGreeting /> */}
+      {/* <PersonList /> */}
+      {/* <StyleSheets /> */}
+      {/* <Modules /> */}
+      {/* <DemoForm /> */}
+      {/* <ParentLifecycle /> */}
+      {/* <RefsDemo /> */}
+      {/* <UserProvider value="Nabendu">
+        <GrandParent />
+      </UserProvider> */}
+      <PostList />
+      {/*<PostForm />*/}
+    </div>
+  );
+}
+
+export default App;
